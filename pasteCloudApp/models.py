7db98292aedb6fd4b4e8a6ee23fd163d996fb90f
konradgnat <konradmgnat@gmail.com>
2019-03-30 17:51:24 -0500

    migrations

--- pasteCloudApp/models.py
+++ pasteCloudApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=200)
+    user_created = models.DateTimeField('date user created')
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    paste_text = models.TextField(blank=True, null=True)
+    paste_created = models.DateTimeField('date paste created')
+    paste_modified = models.DateTimeField('date paste modified')

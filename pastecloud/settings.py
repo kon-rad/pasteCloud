8014e24f5906b51f7c92378bda61ae9d552296f1
konradgnat <konradmgnat@gmail.com>
2019-04-02 22:00:29 -0500

    react

--- pastecloud/settings.py
+++ pastecloud/settings.py
@@ -122,3 +122,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = (
+       'localhost:3000',
+)
+
+STATICFILES_DIRS = (
+    os.path.join(os.path.join(BASE_DIR, 'frontend'), 'build', 'static'),
+    os.path.join(os.path.join(BASE_DIR, 'frontend'), 'build'),
+)
+
+REACT_APP = os.path.join(BASE_DIR, 'frontend')
